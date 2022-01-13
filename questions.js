0436be2975bb90067cac7d05aa118939cf7b6945
Jikebo Kakwaya <95219452+jiks869@users.noreply.github.com>
2022-01-13 18:40:05 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,81 @@
+
+var questions = [{
+    question: "What does CSS stand for?",
+    option1: "Compact Style Sheet",
+    option2: "Computer Screen Sheet",
+    option3: "Complex Style Sheet",
+    option4: "Cascading Style Sheet",
+    answer: "3"
+  },
+  {
+    question: "The  ____ position property is used to define the initial position of the background image.",
+    option1: "background",
+    option2: "margin",
+    option3: "style",
+    option4: "front",
+    answer: "0"
+  },
+  {
+    question: "Which HTML tag us used to define an internal style sheet",
+    option1: "<style>",
+    option2: "<css>",
+    option3: "<script>",
+    option4: "<js>",
+    answer: "0"
+  },
+  {
+    question: "Which HTML attribute is used to define inline styles?",
+    option1: "font",
+    option2: "class",
+    option3: "styles",
+    option4: "style",
+    answer: "3"
+  },
+  {
+    question: "Javascript Code can be called by using",
+    option1: "RMI",
+    option2: "Triggering Event",
+    option3: "PrepProcessor",
+    option4: "Function/Method",
+    answer: "3"
+  },
+  {
+    question: "Correct HTML tag for the largest heading is:",
+    option1: "<head>",
+    option2: "<h6>",
+    option3: "<heading>",
+    option4: "<h1>",
+    answer: "3"
+  },  {
+    question: "The attribute of <form> tag:",
+    option1: "Method",
+    option2: "Action",
+    option3: "Method and Action",
+    option4: "None",
+    answer: "2"
+  },
+  {
+    question: "What is the purpose of a return statement in a function?",
+    option1: "Returns the value and continues executing rest of the statements, if any",
+    option2: "Returns the value and stops the program",
+    option3: "Returns the value and stops executing the function",
+    option4: "Stops executing the function and returns value",
+    answer: "3"
+  },
+  {
+    question: "Web pages starts with which of the following tags?",
+    option1: "<body>",
+    option2: "<title>",
+    option3: "<html>",
+    option4: "<form>",
+    answer: "2"
+  },
+  {
+    question: "Which tag creates number/order list?",
+    option1: "<ul",
+    option2: "<il>",
+    option3: "<ot>",
+    option4: "<ol>",
+    answer: "3"
+  }
+];
